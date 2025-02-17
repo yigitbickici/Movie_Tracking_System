9f960e5fcb1620dc6fe39c2f1a3c3514fddd272e
yigitbickici <99952042+yigitbickici@users.noreply.github.com>
2025-02-17 13:36:57 +0300

    initialize

---
+++ Movie_Management_System/src/main/java/com/Movie_Management_System/spring/MovieManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Movie_Management_System.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Movie_Management_System/src/test/java/com/Movie_Management_System/spring/MovieManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Movie_Management_System.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
