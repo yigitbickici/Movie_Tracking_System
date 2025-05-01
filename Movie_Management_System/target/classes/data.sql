032414baa18ab1ce1abb2b4c4206f693599c693c
yigitbickici <99952042+yigitbickici@users.noreply.github.com>
2025-05-01 21:02:04 +0300

    Revert "Merge branch 'main' of https://github.com/yigitbickici/Movie_Tracking_System"
    This reverts commit 379ca742cad3a2eb52669403bb48c7fb08118c47, reversing
    changes made to d628fc5937d27808364b38202e45dae0f5e16953.

--- Movie_Management_System/target/classes/data.sql
+++
@@ -1,8 +0,0 @@
--- Test kullanıcıları için şifreler: 'admin123', 'user123', 'editor123'
--- BCrypt ile hashlenmiş hali
-INSERT INTO users (id, username, email, password, role) VALUES 
-(1, 'Admin', 'admin@example.com', '$2a$10$3B2yuzQDkP6U5P4v1q1/EOyJHGqN8MtV.eR4YFScOqRo7HpMKc5HK', 'ADMIN'),
-(2, 'User1', 'user@example.com', '$2a$10$ufX5.x4hH4CIIZDkUURsn.ZQF9xmO3NyRGkM8xHRRZK2p.nLiFQS6', 'CUSTOMER'),
-(3, 'Editor', 'editor@example.com', '$2a$10$XTcwjZIgzP40.SAeAbkLUO6njJ.WKQP1vSuFGS60SZ2o.6Kze1v42', 'EDITOR'); 
-
-UPDATE users SET password = '$2a$10$yNrHaGSBzUdb7wSPv5wXl.X9YnICssibqUm6E9afyRFOvDy2F7wGO' WHERE username = 'your_username'; 
